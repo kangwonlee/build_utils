7a656637f38bf2445d93bcb3f833da213ec42bcf
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2019-06-10 14:46:06 +0900

    test import space

--- tests/test_build_util.py
+++ tests/test_build_util.py
@@ -11,11 +11,8 @@
 import os
 import sys
 
-import pytest
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
-
 import IPython.testing.globalipapp
+import pytest
 
 
 @pytest.fixture
@@ -30,3 +27,31 @@ def ipython():
 
 def test_ipython(ipython):
     assert isinstance(ipython, IPython.terminal.interactiveshell.TerminalInteractiveShell), type(ipython)
+
+
+@pytest.fixture
+def ipython_with_build_util(ipython):
+
+    ipython.run_cell(
+        'import os\n'
+        'import sys\n'
+        'sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.getcwd()))))\n'
+        'import build_util.build_util as bu\n'
+    )
+
+    return ipython
+
+
+def test_ipython_with_build_util(ipython_with_build_util):
+    # https://pmbaumgartner.github.io/blog/testing-ipython-magics/
+    ip = ipython_with_build_util
+
+    result = ip.run_cell(
+        'dir()'
+    )
+
+    assert 'bu' in result.result
+
+
+if "__main__" == __name__:
+    pytest.main()
