2596c28beb20df2bc8628c6d0f67d64e4c012d88
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-06-11 05:05:24 +0900

    added test_suites in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(
+
    name='build_util',
    version='0.1',
    description='Build (and run) C/C++ files using iPython / Jupyter magic commands',
@@ -8,4 +9,6 @@ setup(
    author_email='kangwon@gmail.com',
    packages=['build_util'],
    install_requires=['IPython', 'pytest'],
+   test_suite="build_util.tests.test_all" 
+   # https://setuptools.readthedocs.io/en/latest/setuptools.html#test-build-package-and-run-a-unittest-suite
 )
